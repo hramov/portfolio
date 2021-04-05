39f1dbc852b078a887a3f5c947be8145f9a17230
Hramov <trykhramov@gmail.com>
2021-04-05 18:19:13 +0700

    Release

---
+++ dist/precache-manifest.798dabde39b0b28d7b48a6fe3879343b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e117cfd90a8ae12874",
+    "url": "/css/app.21b65a4d.css"
+  },
+  {
+    "revision": "7ebca1ac4c59a39f078adb797ad26b45",
+    "url": "/img/300x200.png"
+  },
+  {
+    "revision": "c8fd82986504b546c90ebedd71b26654",
+    "url": "/img/ais.jpg"
+  },
+  {
+    "revision": "66d9ca104c3a8801913c6bd469562501",
+    "url": "/img/altay.jpg"
+  },
+  {
+    "revision": "30525e7881915563d60c12e17b3b31df",
+    "url": "/img/battleship.png"
+  },
+  {
+    "revision": "005ddc3605a2692c1472c0deba9a5d71",
+    "url": "/img/era.jpeg"
+  },
+  {
+    "revision": "9a80cc2088d4277a0a28a00d821bbceb",
+    "url": "/img/era2.jpg"
+  },
+  {
+    "revision": "cd570c94eafe503251465b0940000d8b",
+    "url": "/img/gazprombank.jpg"
+  },
+  {
+    "revision": "b39a253dbcacc285818b4e6514f8dea1",
+    "url": "/img/genius.jpeg"
+  },
+  {
+    "revision": "8fdcb23720782549ea23f9e451f31a94",
+    "url": "/img/me.jpg"
+  },
+  {
+    "revision": "b59e43068360e157a8792f1e7b92df81",
+    "url": "/img/newser.jpg"
+  },
+  {
+    "revision": "594b078e39b30a0aede96324e8a8aa7e",
+    "url": "/img/rzd.png"
+  },
+  {
+    "revision": "d972b67b28adde3147ada67f8fa0b656",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33e117cfd90a8ae12874",
+    "url": "/js/app.e6125950.js"
+  },
+  {
+    "revision": "93b5b498a667700d13ff",
+    "url": "/js/chunk-vendors.dc7377de.js"
+  },
+  {
+    "revision": "c1884c06f44f042018213e5705cbafcf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f74abc2b6c7a9842e4bfc94889624da8",
+    "url": "/styles/pricing.css"
+  }
+]);
