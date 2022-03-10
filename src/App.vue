201c5b0d02fa682abe87df8554ed24e13517fc4d
hramov <trykhramov@gmail.com>
2022-03-10 17:28:12 +0700

    10.03.2022 update

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,36 @@
 <template>
-  <Header />
-  <br />
-  <main>
-    <router-view />
-  </main>
+  <!-- <Header /> -->
+  <div class="row" style="max-width: 100%">
+    <div class="col-xl-2 col-lg-6 col-md-6 col-sm-12">
+      <Sidebar />
+    </div>
+    <div class="col-xl-10 col-lg-6 col-md-6 col-sm-12">
+      <div class="container-fluid">
+        <router-view />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import Header from "./components/Header";
-
+import "./assets/css/sidebar.css";
+import "./assets/css/bootstrap.min.css";
+// import Header from "./components/Header";
+import Sidebar from "./components/Sidebar.vue";
 export default {
   data() {
     return {};
   },
   components: {
-    Header,
+    // Header,
+    Sidebar,
   },
 };
 </script>
 
 <style>
 * {
-  font-family: 'Raleway', sans-serif;
+  font-family: "Raleway", sans-serif;
   margin: 0;
   padding: 0;
 }
@@ -33,5 +42,17 @@ h2 {
 p {
   text-align: justify;
 }
+.bd-placeholder-img {
+  font-size: 1.125rem;
+  text-anchor: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
+}
 
+@media (min-width: 768px) {
+  .bd-placeholder-img-lg {
+    font-size: 3.5rem;
+  }
+}
 </style>
